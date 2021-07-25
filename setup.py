4a407282b1c1428e38c8b37ce57dfc44c3f92f85
Jules Huisman <huisman.jules@gmail.com>
2021-07-25 13:32:57 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tap-google-search-console",
+    version="0.0.1",
+    author="Jules Huisman",
+    author_email="jules.huisman@quantile.nl",
+    description="A Singer.io tap for Google Search Console",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/quantile-development/tap-google-search-console",
+    project_urls={
+        "Bug Tracker": "https://github.com/quantile-development/tap-google-search-console/issues",
+    },
+    install_requires=[
+        'singer-sdk==0.3.3',
+        'google-api-python-client==2.14.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=['tap_google_search_console'],
+    entry_points="""
+    [console_scripts]
+    tap=tap_google_search_console.tap:TapGoogleSearchConsole.cli
+    tap-google-search-console=tap_google_search_console.tap:TapGoogleSearchConsole.cli
+    """,
+    python_requires=">=3.8",
+)
